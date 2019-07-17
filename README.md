40a4ffefdcbe646e5ce386d5d518ee752264a40f
tho14063 <35983776+tho14063@users.noreply.github.com>
2019-07-17 10:48:00 -0600

    Changed LED control to accept serial information from the raspberry pi with our own protocol. The arduino now saves all of the information to an SD card to read from later.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # POVGlobe
-A POV Globe controlled by a raspberry pi and arduino
+A POV Globe controlled by a raspberry pi and arduino.
 
 The arduino uses the FastLED library to drive the LEDs.
 
 Send data serially from the pi to the arduino.
+We haven't been able to fine tune sending the data, so for now the arduino will save
+the information sent from the pi to an SD card and will then read from the SD card in
+order to display.
 
 Pi processes pictures/video/internet input.
--- led_control/led_control.ino
+++ led_control/led_control.ino
@@ -1,91 +1,134 @@
 #include "FastLED.h"
-#include <SPI.h>
+//#include <SPI.h>
 #include <SD.h>
 
 #define NUM_LEDS 51
-const int dataline = 3;
-const int chipselect = 10;
+#define NUM_COLS 68
+#define NUM_COLORS 3
+#define BRIGHTNESS 25
+#define dataline 3
+#define chipselect 10
 CRGB leds[NUM_LEDS];
 
 File myFile;
 
-//int colorArray[51][3] = {{177, 18, 18}, {177, 18, 18}, {177, 18, 18}, {177, 18, 18}, {177, 18, 18}, {177, 18, 18}, {138, 14, 14},{81, 8, 8},{44, 5, 5},{44, 5, 5},{44, 5, 5},{44, 5, 5},
-//                         {44, 5, 5},{44, 5, 5},{44, 5, 5},{44, 5, 5},{44, 5, 5},{44, 5, 5},{92, 9, 9},{160,16,16},{176,18,18},{177, 18, 18},{177, 18, 18},{177, 18, 18},{177, 18, 18},{255, 255, 255},
-//                         {177, 18, 18},{177, 18, 18},{177, 18, 18},{177, 18, 18},{177, 18, 18},{176, 18, 18},{160, 16, 16},{112, 11, 11},{64, 6, 6},{44, 5, 5},{44, 5, 5},{44, 5, 5},{44, 5, 5},{44, 5, 5},
-//                         {44, 5, 5},{44, 5, 5},{66,7,7},{134,14,14},{172,18,18},{177,18,18},{177,18,18},{177,18,18},{177,18,18},{177,18,18},{255,255,255}};
-
 void setup() {
-  Serial.begin(115200); 
-  LEDS.addLeds<WS2812B, dataline, RGB>(leds, NUM_LEDS);
-  FastLED.setBrightness(100);
-  for (int p=0;p< NUM_LEDS;p++){
-    leds[p] = CRGB::Black;  
-    FastLED.show();
+  Serial.begin(115200);
+  while (!Serial); // wait for serial port. 
+  LEDS.addLeds<PL9823, dataline>(leds, NUM_LEDS);
+//  LEDS.addLeds<WS2812B, dataline, RGB>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+//  for (int p=0;p< NUM_LEDS;p++){
+//    leds[p] = CRGB::Black;  
+//    FastLED.show();
   }
 
   if (!SD.begin(chipselect)) {
     Serial.println("initialization failed!");
     while (1);
   }
-}
-
-// Helper macro to merge 4 bytes
-#define TO_INT32(a,b,c,d) (((d)<<24)|((c)<<16)|((b)<<8)|(a))
-int32_t read_int()
-{
-  uint32_t result[4];
-  for (int i = 0; i < 4; i++) {
-    // Wait for data at the port
-    while (!Serial.available());
-    result[i] = Serial.read();
-  }
-  return TO_INT32(result[0], result[1], result[2], result[3]);
+  // remove previous image and debug files
+  SD.remove("LEDtest.txt");
+  SD.remove("Debug.txt");
 }
 
 int serialReadBlocking() {
   while (!Serial.available()) {}
   int value = 0;
-  do{
-    value = Serial.read();
-  }
-  while (value == ' ');
+  value = Serial.read();
   return value;
 }
 
-void serialReadBlocking(int32_t values[]) {
-  while (!Serial.available()) {}
+void serialReadBlocking(int values[])
+{
   for (int i = 0; i < 3; i++)
   {
-    values[i] = read_int();
+    while (!Serial.available()) {}
+    values[i] = Serial.read();
   }
 }
 
 void loop() {
-  int32_t values[3] = {0, 0, 0};
-  while (serialReadBlocking() != 1) {} 
-  for (long i=0; i < NUM_LEDS; i++) {
-//    leds[i].r = serialReadBlocking();
-//    leds[i].g = serialReadBlocking();
-//    leds[i].b = serialReadBlocking();
-    serialReadBlocking(values);
-//    colorArray[i][0] = values[0];
-//    colorArray[i][1] = values[1];
-//    colorArray[i][2] = values[2];
-    myFile = SD.open("LEDtest.txt", FILE_WRITE);
+  int values[NUM_COLORS] = {0, 0, 0};
+  bool done = false;
+  int num_inputs = 0;
+
+  // open the file
+  myFile = SD.open("LEDtest.txt", FILE_WRITE);
+  
+  // get the picture from the serial line.
+  while (!done)
+  {
+    // block until a 1 is received.
+//    while (serialReadBlocking() != 1) {}
+     
+    for (int i=0; i < NUM_LEDS; i++) 
+    {
+      serialReadBlocking(values);
+
+      if (values[0] == 0 && values[1] == 1 && values[2] == 2) // receive a flag to end loop.
+      {
+        done = true;
+        break;
+      }
+
+      // print the values read fromt the serial port to the SD card.
+      if (myFile)
+      {
+        myFile.println(values[0]);
+        myFile.println(values[1]);
+        myFile.println(values[2]);
+      }
+      num_inputs++;
+    }
+  }
+
+  int totalSize = NUM_LEDS * NUM_COLS;
+  if (myFile)
+  {
+    // Fill in the rest of the file with 1's to fit the needed array size.
+    while (num_inputs < (totalSize))
+    {
+      myFile.println(1);
+      myFile.println(1);
+      myFile.println(1);
+      num_inputs++;
+    }
+    // close the file
+    myFile.close();
+  }
+
+
+  myFile = SD.open("Debug.txt", FILE_WRITE);
+  if (myFile)
+  {
+    myFile.println("Finished  writing");
+    myFile.close();
+  }
+
+
+  myFile = SD.open("LEDtest.txt");
+  // display the picture.
+  while(1)
+  {
+    myFile.seek(0);
     if (myFile)
     {
-      myFile.print(values[0]);
-      myFile.print(' ');
-      myFile.print(values[1]);
-      myFile.print(' ');
-      myFile.println(values[2]);
-      myFile.close();
+      for(int j = 0; j < NUM_COLS; j++)
+      {
+        for(int i = 0; i < NUM_LEDS; i++)
+        {
+          int red = myFile.parseInt();
+          int green = myFile.parseInt();
+          int blue = myFile.parseInt();
+          leds[i].setRGB(red, green, blue);
+        }
+        FastLED.show();
+      }
     }
-    leds[i].setRGB(values[0], values[1], values[2]);
-   }
-//  for(int i = 0; i < NUM_LEDS; i++)
-//  {
-//    leds[i].setRGB(colorArray[i][0], colorArray[i][1], colorArray[i][2]);
-//  }
-  FastLED.show();
+  }
+  if (myFile)
+  {
+    myFile.close();
+  }
 }
